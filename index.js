6eb568d93eb39b21506a0f0676876a45bf124783
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-15 14:53:41 -0400

    commit tree ab5bf89fd1a9346c93dc7924277dc1eefb40bb0f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(sourceArray, func) {
+    let newArray = [];
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(func(sourceArray[i]))       //
+    }
+    return newArray
+}
+
+function reduce(sourceArray, func, startingPoint) {
+    let val = undefined
+    
+    if (startingPoint) {
+        val = startingPoint;  
+    } else {
+    val = sourceArray[0];
+    }
+
+    for (let i =  startingPoint ? 0 : 1; i < sourceArray.length; i++) {
+        val = func(sourceArray[i], val)
+    }
+    return val;
+}
